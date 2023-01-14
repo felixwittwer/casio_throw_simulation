bd07ac76bd9c4c798f04cd404961d7a777fc12f1
Felix Wittwer <120730441+felixwittwer@users.noreply.github.com>
2023-01-14 17:47:54 +0100

    Add change values

--- THROW.c
+++ THROW.c
@@ -1,26 +1,39 @@
-/*****************************************************************/
-/*                                                               */
-/*   CASIO fx-9860G SDK Library                                  */
-/*                                                               */
-/*   File name : [ProjectName].c                                 */
-/*                                                               */
-/*   Copyright (c) 2006 CASIO COMPUTER CO., LTD.                 */
-/*                                                               */
-/*****************************************************************/
+/*************************************************************/
+/** throwing calculation and simulation                     **/
+/**                                                         **/
+/** @author Felix Wittwer                                   **/
+/**                                                         **/
+/** @file THROW.c                                           **/
+/**                                                         **/
+/** @date 01-14-2023                                        **/
+/*************************************************************/
+#include <stdio.h>
+#include <math.h>
 #include "fxlib.h"
 
-void IL_Print_Float(int x, int y, int f){
+void Print_Float(int x, int y, float f, int mini){
     unsigned char buffer[9];
     sprintf(buffer, "%f", f);
-    PrintXY(x,y,buffer,0);
+    if(mini==0){
+        PrintXY(x,y,buffer,0);
+    }else if(mini==1){
+        PrintMini(x,y,buffer, MINI_OVER);
+    }
 }
 
-void Render_Arrows(int x, int y){
-    unsigned char up[3]={0xE6,0x9C,0};
-    unsigned char down[3]={0xE6,0x9D,0};
+void Print_Int(int x, int y, int i, int mini){
+    unsigned char buffer[9];
+    sprintf(buffer, "%d", i);
+    if(mini==0){
+        PrintXY(x,y,buffer,0);
+    }else if(mini==1){
+        PrintMini(x,y,buffer, MINI_OVER);
+    }
+}
 
-    PrintXY(x, y-6, up, 0);
-    PrintXY(x, y+8, down, 0);
+void Render_Indacator(int x, int y){
+    unsigned char up[3]={0xE6,0x91,0};
+    PrintXY(x, y, up, 0);
 }
 
 void Render_F_Button(int x, int y, unsigned char *str){
@@ -35,30 +48,36 @@ void Render_F_Button(int x, int y, unsigned char *str){
     Bdisp_SetPoint_VRAM(x+17, y+6, 0);
 }
 
-void Initialize(){
+void Render_Main(){
     unsigned char alpha[3]={0xE6,0x40,0};
     unsigned char zero[3]={0xE5,0xD0,0};
 
     Bdisp_AllClr_DDVRAM();
 
     PrintMini(2,2, (unsigned char*)"Throw simulation",MINI_OVER);
-    Render_F_Button(2,58, "EXIT");
-    Render_F_Button(24,58, "DEL ");
-    Render_F_Button(46,58, "CALC");
-    Render_F_Button(68,58, "SIM ");
+    Render_F_Button(1,58, "EXIT");
+    Render_F_Button(23,58, "DEL ");
+    Render_F_Button(45,58, "CALC");
+    Render_F_Button(67,58, "SIM ");
+    Render_F_Button(89,58, "HELP");
 
-    PrintXY(5,16, alpha, 0);
-    PrintXY(17,17, (unsigned char*)"=",0);
+    PrintXY(11,16, alpha, 0);
+    PrintXY(23,17, (unsigned char*)"=",0);
     
-    PrintXY(10,27, zero, 0);
-    PrintXY(5,26, (unsigned char*)"v",0);
-    PrintXY(17,27, (unsigned char*)"=",0);
+    PrintXY(16,27, zero, 0);
+    PrintXY(11,26, (unsigned char*)"v",0);
+    PrintXY(23,27, (unsigned char*)"=",0);
 
-    PrintXY(5,37, (unsigned char*)"g",0);
-    PrintXY(17,37, (unsigned char*)"=",0);
-
+    PrintXY(11,37, (unsigned char*)"g",0);
+    PrintXY(23,37, (unsigned char*)"=",0);
 
     Bdisp_PutDisp_DD();
+}
+
+void Render_Var(angle, startvelocity, gravitationalforce){
+    Print_Float(31, 37, gravitationalforce, 0);
+    Print_Int(31, 16, angle, 0);
+    Print_Int(31, 26, startvelocity, 0);
 }
 
 
@@ -85,7 +104,8 @@ int AddIn_main(int isAppli, unsigned short OptionNum)
     float gravitationalforce;
 
     Bdisp_AllClr_DDVRAM();
-    Initialize();
+    Render_Main();
+    Render_Indacator(3,16);
 
     angle = 0;
     startvelocity = 0;
@@ -93,26 +113,53 @@ int AddIn_main(int isAppli, unsigned short OptionNum)
 
     while(1){
 	    GetKey(&key);
-        Initialize();
+        Render_Main();
+        Render_Var(angle, startvelocity, gravitationalforce);
+
+        if(key==KEY_CTRL_DOWN){
+            varselected = varselected+1;
+        }else if(key==KEY_CTRL_UP){
+            varselected = varselected-1;
+        }
+
+        if(varselected==1){
+            Render_Indacator(3,16);
+        }else if(varselected==2){
+            Render_Indacator(3,26);
+        }else if(varselected==3){
+            Render_Indacator(3,37);
+        }else if(varselected <1||varselected>3){
+            varselected = 1;
+            Render_Indacator(3,16);
+        }
+
+        if(key==KEY_CTRL_RIGHT&&varselected==1){
+            angle = angle+1;
+        }else if(key==KEY_CTRL_RIGHT&&varselected==2){
+            startvelocity = startvelocity+1;
+        }
+        Render_Var(angle, startvelocity, gravitationalforce);
+
 
         if(key==KEY_CTRL_F2){
             PopUpWin(4);
             PrintXY(12,8, (unsigned char*)"Delete All?",0);
             PrintXY(30,24, (unsigned char*)"Yes:[F1]",0);
             PrintXY(30,32, (unsigned char*)"No :[F6]",0);
-            if(key==KEY_CTRL_F1){
-                break;
-            }else if(key==KEY_CTRL_F6){
-                angle = 0;
-                startvelocity = 0;
-                gravitationalforce = 9.81;
-                break;
+            while(1){
+                GetKey(&key);
+                if(key==KEY_CTRL_F1){
+                    break;
+                }else if(key==KEY_CTRL_F6){
+                    angle = 0;
+                    startvelocity = 0;
+                    gravitationalforce = 9.81;
+                    varselected = 1;
+                    break;
+                }
             }
+            Render_Main();    
         }
-
-        // Render_Arrows(24, 16);
-
-
     }
 
     return 1;
@@ -138,7 +185,7 @@ unsigned long BR_Size;
 #pragma section _TOP
 
 //****************************************************************************
-//  InitializeSystem
+//  Render_MainSystem
 //
 //  param   :   isAppli   : 1 = Application / 0 = eActivity
 //              OptionNum : Option Number (only eActivity)
@@ -146,7 +193,7 @@ unsigned long BR_Size;
 //  retval  :   1 = No error / 0 = Error
 //
 //****************************************************************************
-int InitializeSystem(int isAppli, unsigned short OptionNum)
+int Render_MainSystem(int isAppli, unsigned short OptionNum)
 {
     return INIT_ADDIN_APPLICATION(isAppli, OptionNum);
 }
