f6c769a1ed0abfe5d0112cebdb517acf2aa63b4f
Felix Wittwer <120730441+felixwittwer@users.noreply.github.com>
2023-01-14 10:12:17 +0100

    Initial commit

---
+++ THROW.c
@@ -0,0 +1,127 @@
+/*****************************************************************/
+/*                                                               */
+/*   CASIO fx-9860G SDK Library                                  */
+/*                                                               */
+/*   File name : [ProjectName].c                                 */
+/*                                                               */
+/*   Copyright (c) 2006 CASIO COMPUTER CO., LTD.                 */
+/*                                                               */
+/*****************************************************************/
+#include "fxlib.h"
+
+void IL_Print_Float(int x, int y, int f){
+    unsigned char buffer[9];
+    sprintf(buffer, "%f", f);
+    PrintXY(x,y,buffer,0);
+}
+
+void Render_Arrows(int x, int y){
+    unsigned char up[3]={0xE6,0x9C,0};
+    unsigned char down[3]={0xE6,0x9D,0};
+
+    PrintXY(x, y-6, up, 0);
+    PrintXY(x, y+8, down, 0);
+}
+
+void Render_F_Button(int x, int y, unsigned char *str){
+    Bdisp_DrawLineVRAM(x, y-1, x+17, y-1);
+    Bdisp_DrawLineVRAM(x-1, y-1, x-1, y+5);
+    Bdisp_DrawLineVRAM(x+17, y-1, x+17, y+5);
+    Bdisp_DrawLineVRAM(x+16, y-1, x+16, y+5);
+    PrintMini(x,y, str, MINI_REV);
+
+    Bdisp_ClearLineVRAM(x+17, y+4, x+15, y+6);
+    Bdisp_ClearLineVRAM(x+17, y+5, x+16, y+6);
+    Bdisp_SetPoint_VRAM(x+17, y+6, 0);
+}
+
+void Initialize(){
+    unsigned char alpha[3]={0xE6,0x40,0};
+
+    PrintMini(2,2, (unsigned char*)"Throw simulation",MINI_OVER);
+    Render_F_Button(2,58, "EXIT");
+    Render_F_Button(24,58, "DEL ");
+    Render_F_Button(46,58, "CALC");
+    Render_F_Button(68,58, "SIM ");
+	PrintXY(5,16, alpha, 0);
+    PrintXY(12,17, (unsigned char*)"=",0);
+    Bdisp_PutDisp_DD();
+}
+
+
+//****************************************************************************
+//  AddIn_main (Sample program main function)
+//
+//  param   :   isAppli   : 1 = This application is launched by MAIN MENU.
+//                        : 0 = This application is launched by a strip in eACT application.
+//
+//              OptionNum : Strip number (0~3)
+//                         (This parameter is only used when isAppli parameter is 0.)
+//
+//  retval  :   1 = No error / 0 = Error
+//
+//****************************************************************************
+int AddIn_main(int isAppli, unsigned short OptionNum)
+{
+    unsigned int key;
+
+    int varselected = 1;
+
+    unsigned int angle;
+    unsigned int startvelocity;
+    float gravitationalforce;
+
+    Bdisp_AllClr_DDVRAM();
+    Initialize();
+
+    angle = 0;
+    startvelocity = 0;
+    gravitationalforce = 9.81;
+
+    while(1){
+	    GetKey(&key);
+        Initialize();
+
+        // Render_Arrows(24, 16);
+
+
+    }
+
+    return 1;
+}
+
+
+
+
+//****************************************************************************
+//**************                                              ****************
+//**************                 Notice!                      ****************
+//**************                                              ****************
+//**************  Please do not change the following source.  ****************
+//**************                                              ****************
+//****************************************************************************
+
+
+#pragma section _BR_Size
+unsigned long BR_Size;
+#pragma section
+
+
+#pragma section _TOP
+
+//****************************************************************************
+//  InitializeSystem
+//
+//  param   :   isAppli   : 1 = Application / 0 = eActivity
+//              OptionNum : Option Number (only eActivity)
+//
+//  retval  :   1 = No error / 0 = Error
+//
+//****************************************************************************
+int InitializeSystem(int isAppli, unsigned short OptionNum)
+{
+    return INIT_ADDIN_APPLICATION(isAppli, OptionNum);
+}
+
+#pragma section
+
